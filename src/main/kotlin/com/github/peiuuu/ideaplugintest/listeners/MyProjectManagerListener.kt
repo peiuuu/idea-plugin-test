24b25b4aa579ad2a5a38b4d9db26e15c0ab7fd2b
GitHub Action <action@github.com>
2020-11-02 09:10:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/peiuuu/ideaplugintest/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/peiuuu/ideaplugintest/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.peiuuu.ideaplugintest.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.peiuuu.ideaplugintest.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/peiuuu/ideaplugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/peiuuu/ideaplugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.peiuuu.ideaplugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.peiuuu.ideaplugintest.MyBundle
 
 class MyApplicationService {
 
